ba0cf24a7dc00ac573d8a28d2ff88ab2ffef3b6c
belonogo <belonozhko99@ya.ru>
2022-02-17 18:07:07 +0300

    1

--- spi_test.py
+++ spi_test.py
@@ -1,26 +1,4 @@
-import spidev
-import time
+import wiring_controls as wc
 
-SPI_BUS = 0          # spidev0
-SPI_SS  = 0          # spidev0.0
-SPI_CLOCK = 1000000  # 1 Mhz
-
-# setup SPI
-spi = spidev.SpiDev(SPI_BUS, SPI_SS)
-spi.max_speed_hz = SPI_CLOCK
-
-# transfer 2 bytes at a time, ^C to exit
-try:
-    v = 0
-    while True:
-        send = [v, v+1]
-        print("")
-        print("TX:", send)
-        print("RX:", spi.xfer(send))
-        time.sleep(0.5)
-        if v >= 254:
-            v = 0
-        else:
-            v = (v+2)
-finally:
-    spi.close()
+wc = wc.WiringControls()
+wc.digital_write(10, 1)
