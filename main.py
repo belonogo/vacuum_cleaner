115111da628833590708efb4f509c8b9d6e8c939
lolko40rus <74459438+lolko40rus@users.noreply.github.com>
2022-02-21 22:14:41 +0300

    add blinking (task 10)

--- main.py
+++ main.py
@@ -100,12 +100,10 @@ class SensorScreen(Screen):
         self.ids.fuel_level_sensor.text = "Уровень топлива: {} %".format(
             int(self.convert_ohm_to_temp(0, 100, 10, 1000, fuel_level)))
         FUEL_CURRENT_LEVEL = int(self.convert_ohm_to_temp(0, 100, 10, 1000, fuel_level))
-        if FUEL_CURRENT_LEVEL/FUEL_CRITICAL_LEVEL <= 15:
-            pass
-            #self.ids.indicator_gasoline.opacity = 1.0
-        else:
-            pass
-            #self.ids.indicator_gasoline.opacity = 0.0
+        if FUEL_CURRENT_LEVEL/FUEL_CRITICAL_LEVEL <= 15 and self.ids.indicator_gasoline.opacity == 0.0:
+            self.ids.indicator_gasoline.opacity = 1.0
+        elif FUEL_CURRENT_LEVEL/FUEL_CRITICAL_LEVEL > 15 and self.ids.indicator_gasoline.opacity == 1.0:
+            self.ids.indicator_gasoline.opacity = 0.0
 
         self.ids.engine_temp_sensor.text = "Температура ОЖ: {} С°".format(
             int(self.convert_ohm_to_temp(30, 120, 100, 1000, engine_temp)))
