3a95ee937c9329a3e05a1d24cf9ffc28e3142def
belonogo <belonozhko99@ya.ru>
2022-02-23 14:12:27 +0300

    add shutdown after power off

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class BaseScreen(Screen):
 
     def check_power_thread(self):
         while not app.stop_event.is_set():
-            power_state = self.wc.analog_read(wc.POWER_CHECK_PIN)
+            power_state = self.wc.digital_read_exp(wc.POWER_CHECK_PIN)
             self.ids.test_text.text = "{}".format(power_state)
             if power_state == 0:
                 pass
